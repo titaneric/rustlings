9117628b8866ea10529f76880137b11b766313b4
titaneric <chenyihuang001@gmail.com>
2020-08-02 16:12:26 +0800

    :penguin: Finish clippy2

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,13 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
+
+use std::f64::EPSILON;
 
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    if (y - x).abs() > EPSILON {
         println!("Success!");
     }
 }
